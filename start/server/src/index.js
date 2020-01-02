47faba6d3cdc5ce9e2aac53e6157172777357747
guandjoy <guandjoy@gmail.com>
2020-01-02 22:17:28 +0200

    2. Hook up your data sources

---
+++ start/server/src/index.js
@@ -0,0 +1,20 @@
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const { createStore } = require("./utils");
+
+const LaunchAPI = require("./datasources/launch");
+const UserAPI = require("./datasources/user");
+
+const store = createStore();
+
+const server = new ApolloServer({
+  typeDefs,
+  dataSources: () => ({
+    launchAPI: new LaunchAPI(),
+    userAPI: new UserAPI({ store })
+  })
+});
+
+server.listen().then(({ url }) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
