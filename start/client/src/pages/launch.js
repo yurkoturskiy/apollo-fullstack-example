0366e9667b68e876f129f536ba7748dbb9f05df6
Guandjoy <guandjoy@gmail.com>
2020-01-27 00:32:36 +0200

    6. Fetch data with queries

--- start/client/src/pages/launch.js
+++ start/client/src/pages/launch.js
@@ -1,5 +1,51 @@
-import React from 'react';
+import React, { Fragment } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
-export default function Launch() {
-  return <div />;
-}
+import { Loading, Header, LaunchDetail } from "../components";
+import { ActionButton } from "../containers";
+
+export const GET_LAUNCH_DETAILS = gql`
+  query LaunchDetails($launchId: ID!) {
+    launch(id: $launchId) {
+      id
+      site
+      isBooked
+      rocket {
+        id
+        name
+        type
+      }
+      mission {
+        name
+        missionPatch
+      }
+    }
+  }
+`;
+
+const Launch = ({ launchId }) => {
+  const { data, loading, error } = useQuery(GET_LAUNCH_DETAILS, {
+    variables: { launchId }
+  });
+
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR: {error.message}</p>;
+  if (!data) return <p>Not found</p>;
+
+  return (
+    <Fragment>
+      <Header
+        image={
+          data.launch && data.launch.mission && data.launch.mission.missionPatch
+        }
+      >
+        {data && data.launch && data.launch.mission && data.launch.mission.name}
+      </Header>
+      <LaunchDetail {...data.launch} />
+      <ActionButton {...data.launch} />
+    </Fragment>
+  );
+};
+
+export default Launch;
--- start/client/src/pages/profile.js
+++ start/client/src/pages/profile.js
@@ -1,5 +1,43 @@
-import React from 'react';
+import React, { Fragment } from "react";
+import { useQuery } from "@apollo/react-hooks";
+import gql from "graphql-tag";
 
-export default function Profile() {
-  return <div />;
-}
+import { Loading, Header, LaunchTile } from "../components";
+import { LAUNCH_TILE_DATA } from "./launches";
+
+export const GET_MY_TRIPS = gql`
+  query GetMyTrips {
+    me {
+      id
+      email
+      trips {
+        ...LaunchTile
+      }
+    }
+  }
+  ${LAUNCH_TILE_DATA}
+`;
+
+const Profile = () => {
+  const { data, loading, error } = useQuery(GET_MY_TRIPS, {
+    fetchPolicy: "network-only"
+  });
+  if (loading) return <Loading />;
+  if (error) return <p>ERROR: {error.message}</p>;
+  if (data === undefined) return <p>ERROR</p>;
+
+  return (
+    <Fragment>
+      <Header>My Trips</Header>
+      {data.me && data.me.trips.length ? (
+        data.me.trips.map(launch => (
+          <LaunchTile key={launch.id} launch={launch} />
+        ))
+      ) : (
+        <p>You haven't booked any trips</p>
+      )}
+    </Fragment>
+  );
+};
+
+export default Profile;
