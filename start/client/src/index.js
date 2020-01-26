530769f8df9bbb95b57e206896cbcd5f62994d38
Yurko Turskiy <guandjoy@gmail.com>
2020-01-26 23:57:00 +0200

    5. Connect your API to a client

---
+++ start/client/src/index.js
@@ -0,0 +1,43 @@
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+import gql from "graphql-tag";
+import { ApolloProvider } from "@apollo/react-hooks";
+import React from "react";
+import ReactDOM from "react-dom";
+import Pages from "./pages";
+import injectStyles from "./styles";
+
+const cache = new InMemoryCache();
+const link = new HttpLink({
+  uri: "http://localhost:4000/"
+});
+
+const client = new ApolloClient({
+  cache,
+  link
+});
+
+// ... above is the instantiation of the client object.
+client
+  .query({
+    query: gql`
+      query GetLaunch {
+        launch(id: 56) {
+          id
+          mission {
+            name
+          }
+        }
+      }
+    `
+  })
+  .then(result => console.log(result));
+
+injectStyles();
+ReactDOM.render(
+  <ApolloProvider client={client}>
+    <Pages />
+  </ApolloProvider>,
+  document.getElementById("root")
+);
